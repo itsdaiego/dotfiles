f1423636f8ec435e048f1af7d92241794996d6ce
Dyego Rodrigo <dyego9444@gmail.com>
2026-06-03 10:54:10 -0300

    dap: show breakpoint emoji for rejected state

--- .vim/lua/plugins/dap.lua
+++ .vim/lua/plugins/dap.lua
@@ -170,6 +170,7 @@ return {
 
       vim.fn.sign_define('DapBreakpoint', {text='🛑', texthl='', linehl='', numhl=''})
       vim.fn.sign_define('DapBreakpointCondition', {text='🔍', texthl='', linehl='', numhl=''})
+      vim.fn.sign_define('DapBreakpointRejected', {text='🛑', texthl='', linehl='', numhl=''})
       vim.fn.sign_define('DapStopped', {text='🔥', texthl='', linehl='', numhl=''})
 
       -- Load telescope-dap extension with error handling and deferral
